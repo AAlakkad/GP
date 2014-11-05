03a43c84cd66096ce4515f3b115ea35b1e5fb8ae
Ammar Alakkad <am.alakkad@gmail.com>
2014-11-05 22:10:00 +0200

    Add many-to-many relation between recipe and ingredient

--- laravel/app/models/Ingredient.php
+++ laravel/app/models/Ingredient.php
@@ -10,4 +10,9 @@ class Ingredient extends \Eloquent {
 	// Don't forget to fill this array
 	protected $fillable = ['name', 'description'];
 
+    public function recipes()
+    {
+        return $this->belongsToMany('Recipe');
+    }
+
 }
--- laravel/app/models/Recipe.php
+++ laravel/app/models/Recipe.php
@@ -4,10 +4,16 @@ class Recipe extends \Eloquent {
 
 	// Add your validation rules here
 	public static $rules = [
-		// 'title' => 'required'
+        'name' => 'required',
+        'description' => 'required',
+		'steps' => 'required',
 	];
 
 	// Don't forget to fill this array
-	protected $fillable = [];
+	protected $fillable = ['name', 'description', 'steps'];
 
+    public function ingredients()
+    {
+        return $this->belongsToMany('Ingredient')
+    }
 }
